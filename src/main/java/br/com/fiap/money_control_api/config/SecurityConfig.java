f70c7fdf5228d15fc86a336c48dc714f884b2ba6
hmarinhoo <hellenmcordeiro@gmail.com>
2025-05-16 08:45:13 -0300

    .

--- src/main/java/br/com/fiap/money_control_api/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/money_control_api/config/SecurityConfig.java
@@ -20,13 +20,14 @@ public class SecurityConfig {
     private AuthFilter authFilter;
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> auth
-                    .requestMatchers(HttpMethod.POST, "/users/**").permitAll()
-                    .requestMatchers(HttpMethod.POST, "/login/**").permitAll()
-                    .anyRequest().authenticated()
-                )
+                        .requestMatchers(HttpMethod.POST, "/users/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/login/**").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable())
                 .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
                 .httpBasic(Customizer.withDefaults())
@@ -35,31 +36,31 @@ public class SecurityConfig {
 
     // @Bean
     // UserDetailsService userDetailsService(){
-    //     var user1 = User
-    //                     .withUsername("joao")
-    //                     .password("$2a$12$GH.8Gz4DeUylAeYFQ3.bFODUSFuu/zQSDGmIqYPLm.VzYn6MHYLKC")
-    //                     .roles("ADMIN")
-    //                     .build();
+    // var user1 = User
+    // .withUsername("joao")
+    // .password("$2a$12$GH.8Gz4DeUylAeYFQ3.bFODUSFuu/zQSDGmIqYPLm.VzYn6MHYLKC")
+    // .roles("ADMIN")
+    // .build();
 
-    //     var user2 = User
-    //                     .withUsername("maria")
-    //                     .password("$2a$12$L.XqfJcp/r4W5.pzAoIipu6q/hx.eo59/i2qLSDqg6OwCVg6biq.O")
-    //                     .roles("USER")
-    //                     .build();
+    // var user2 = User
+    // .withUsername("maria")
+    // .password("$2a$12$L.XqfJcp/r4W5.pzAoIipu6q/hx.eo59/i2qLSDqg6OwCVg6biq.O")
+    // .roles("USER")
+    // .build();
 
-    //     var users = List.of(user1, user2);
+    // var users = List.of(user1, user2);
 
-    //     return new InMemoryUserDetailsManager(users);
+    // return new InMemoryUserDetailsManager(users);
     // }
 
     @Bean
-    PasswordEncoder passwordEncoder(){
+    PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    AuthenticationManager authManager(AuthenticationConfiguration config) throws Exception{
+    AuthenticationManager authManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
-    
+
 }
